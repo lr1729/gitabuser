b7aa1a8f7260eb40247b63f0ca5133b2b9152079
ling1729 <renlingfeng21@gmail.com>
2020-12-02 19:14:25 -0800

    more stuff

--- upload.sh
+++ upload.sh
@@ -5,17 +5,22 @@ declare -a directories=(
   ".gitthree"
 )
 
+# The size limit of one repository before using another in bytes
+sizelimit=96636714682
+
 # Get number of repositories
 repositories=${#directories[@]}
 
+# Check for and create repositories if they don't exist
 for (( i=1; i<${repositories}+1; i++ ));
 do
   if [ ! -d ${directories[$i-1]} ]; then
     echo -n "Directory ${directories[$i-1]} DOES NOT exist. Create repository? (Y/n) "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-      git init .
-      mv .git ${directories[$i-1]}
+      tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+      git init $tmp_dir
+      mv $tmp_dir/.git ${directories[$i-1]}
       git --git-dir=${directories[$i-1]} checkout -b master
       echo -n "Enter a remote for the repository: "
       read answer
@@ -23,4 +28,18 @@ do
     fi
   fi
 done
+
+# Find repository to upload to
+for (( i=1; i<${repositories}+1; i++ ));
+do
+  if [ $(du -s ${directories[$i-1]} | cut -f1) -lt $sizelimit ]; then
+    repository=${directories[$i-1]}
+    break
+  fi
+done
+if [ -z "$repository" ]; then
+  echo "All repositories are full"
+  exit 1
+fi
+echo Saving files to $repository
 
